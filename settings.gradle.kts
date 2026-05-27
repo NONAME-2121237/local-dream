2946d11397ed190dbf5da25f6d72d40a17b00946
NONAME-2121237 <NONAME-2121237@users.noreply.github.com>
2026-05-27 10:07:31 +0000

    feat: 判断SoC是否支持NPU
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,18 @@
 pluginManagement {
     repositories {
+        mavenLocal()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://repo1.maven.org/maven2") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        maven("https://jitpack.io")
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://repo1.maven.org/maven2") }
     }
 }
 
